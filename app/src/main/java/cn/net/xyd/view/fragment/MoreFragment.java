ed0ff4c9fb4aec0b2f7c8ddf3664723caa81d132
Phoenix <raines_lcf@163.com>
2015-09-04 15:59:18 +0800

    初始化

---
+++ app/src/main/java/cn/net/xyd/view/fragment/MoreFragment.java
@@ -0,0 +1,27 @@
+package cn.net.xyd.view.fragment;
+
+import android.content.Context;
+import android.view.View;
+
+import cn.net.xyd.R;
+import cn.net.xyd.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2015/8/24 0024.
+ */
+public class MoreFragment extends BaseFragment {
+    @Override
+    public int bindLayout() {
+        return R.layout.ac_index;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+}
